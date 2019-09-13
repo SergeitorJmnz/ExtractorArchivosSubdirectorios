abcb48b4ce3c4a9468a47eebac17fdeb652e831e
Sergio Jimenez Salmeron <Sergiojim23@gmail.com>
2019-09-13 13:22:52 +0200

    Primera version

---
+++ CopyFilesApp/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class mainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnExtract = New System.Windows.Forms.Button()
+        Me.boxInFolder = New System.Windows.Forms.GroupBox()
+        Me.btnExamIn = New System.Windows.Forms.Button()
+        Me.txtFolderIn = New System.Windows.Forms.TextBox()
+        Me.boxOutFolder = New System.Windows.Forms.GroupBox()
+        Me.btnExamOut = New System.Windows.Forms.Button()
+        Me.txtFolderOut = New System.Windows.Forms.TextBox()
+        Me.tmrExtractBtn = New System.Windows.Forms.Timer(Me.components)
+        Me.prog_bar = New System.Windows.Forms.ProgressBar()
+        Me.boxInFolder.SuspendLayout()
+        Me.boxOutFolder.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnExtract
+        '
+        Me.btnExtract.Location = New System.Drawing.Point(423, 134)
+        Me.btnExtract.Name = "btnExtract"
+        Me.btnExtract.Size = New System.Drawing.Size(101, 34)
+        Me.btnExtract.TabIndex = 0
+        Me.btnExtract.Text = "Extraer"
+        Me.btnExtract.UseVisualStyleBackColor = True
+        '
+        'boxInFolder
+        '
+        Me.boxInFolder.Controls.Add(Me.btnExamIn)
+        Me.boxInFolder.Controls.Add(Me.txtFolderIn)
+        Me.boxInFolder.Location = New System.Drawing.Point(3, 5)
+        Me.boxInFolder.Name = "boxInFolder"
+        Me.boxInFolder.Size = New System.Drawing.Size(521, 56)
+        Me.boxInFolder.TabIndex = 1
+        Me.boxInFolder.TabStop = False
+        Me.boxInFolder.Text = "Carpeta de entrada"
+        '
+        'btnExamIn
+        '
+        Me.btnExamIn.Location = New System.Drawing.Point(437, 19)
+        Me.btnExamIn.Name = "btnExamIn"
+        Me.btnExamIn.Size = New System.Drawing.Size(75, 20)
+        Me.btnExamIn.TabIndex = 3
+        Me.btnExamIn.Text = "Examinar..."
+        Me.btnExamIn.UseVisualStyleBackColor = True
+        '
+        'txtFolderIn
+        '
+        Me.txtFolderIn.Location = New System.Drawing.Point(6, 19)
+        Me.txtFolderIn.Name = "txtFolderIn"
+        Me.txtFolderIn.Size = New System.Drawing.Size(422, 20)
+        Me.txtFolderIn.TabIndex = 1
+        '
+        'boxOutFolder
+        '
+        Me.boxOutFolder.Controls.Add(Me.btnExamOut)
+        Me.boxOutFolder.Controls.Add(Me.txtFolderOut)
+        Me.boxOutFolder.Location = New System.Drawing.Point(3, 67)
+        Me.boxOutFolder.Name = "boxOutFolder"
+        Me.boxOutFolder.Size = New System.Drawing.Size(521, 61)
+        Me.boxOutFolder.TabIndex = 2
+        Me.boxOutFolder.TabStop = False
+        Me.boxOutFolder.Text = "Carpeta de salida"
+        '
+        'btnExamOut
+        '
+        Me.btnExamOut.Location = New System.Drawing.Point(437, 19)
+        Me.btnExamOut.Name = "btnExamOut"
+        Me.btnExamOut.Size = New System.Drawing.Size(75, 20)
+        Me.btnExamOut.TabIndex = 4
+        Me.btnExamOut.Text = "Examinar..."
+        Me.btnExamOut.UseVisualStyleBackColor = True
+        '
+        'txtFolderOut
+        '
+        Me.txtFolderOut.Location = New System.Drawing.Point(6, 19)
+        Me.txtFolderOut.Name = "txtFolderOut"
+        Me.txtFolderOut.Size = New System.Drawing.Size(422, 20)
+        Me.txtFolderOut.TabIndex = 2
+        '
+        'tmrExtractBtn
+        '
+        '
+        'prog_bar
+        '
+        Me.prog_bar.Location = New System.Drawing.Point(9, 144)
+        Me.prog_bar.Name = "prog_bar"
+        Me.prog_bar.Size = New System.Drawing.Size(399, 16)
+        Me.prog_bar.TabIndex = 3
+        Me.prog_bar.Visible = False
+        '
+        'mainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(527, 172)
+        Me.Controls.Add(Me.prog_bar)
+        Me.Controls.Add(Me.boxOutFolder)
+        Me.Controls.Add(Me.boxInFolder)
+        Me.Controls.Add(Me.btnExtract)
+        Me.MaximizeBox = False
+        Me.Name = "mainForm"
+        Me.Text = "Extractor de documentos"
+        Me.boxInFolder.ResumeLayout(False)
+        Me.boxInFolder.PerformLayout()
+        Me.boxOutFolder.ResumeLayout(False)
+        Me.boxOutFolder.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnExtract As Button
+    Friend WithEvents boxInFolder As GroupBox
+    Friend WithEvents btnExamIn As Button
+    Friend WithEvents txtFolderIn As TextBox
+    Friend WithEvents boxOutFolder As GroupBox
+    Friend WithEvents btnExamOut As Button
+    Friend WithEvents txtFolderOut As TextBox
+    Friend WithEvents tmrExtractBtn As Timer
+    Friend WithEvents prog_bar As ProgressBar
+End Class
---
+++ CopyFilesApp/Form1.vb
@@ -0,0 +1,108 @@
+﻿Imports System.IO
+
+Public Class mainForm
+
+#Region "Variables globales"
+
+    Dim fbdIn As New FolderBrowserDialog
+    Dim fbdOut As New FolderBrowserDialog
+
+    Dim g_count As Integer = 1
+
+#End Region
+
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btnExtract.Enabled = False
+        tmrExtractBtn.Start()
+    End Sub
+
+#Region "Eventos del formulario"
+
+#Region "Botones"
+
+    Private Sub BtnExamIn_Click(sender As Object, e As EventArgs) Handles btnExamIn.Click
+
+        If fbdIn.ShowDialog = DialogResult.OK Then
+            txtFolderIn.Text = fbdIn.SelectedPath
+        End If
+
+    End Sub
+
+    Private Sub BtnExamOut_Click(sender As Object, e As EventArgs) Handles btnExamOut.Click
+
+        If fbdOut.ShowDialog = DialogResult.OK Then
+            txtFolderOut.Text = fbdOut.SelectedPath
+        End If
+
+    End Sub
+
+    Private Sub BtnExtract_Click(sender As Object, e As EventArgs) Handles btnExtract.Click
+
+        Dim files() As String = IO.Directory.GetFiles(txtFolderIn.Text, "*.*", IO.SearchOption.AllDirectories)
+        Dim outputFolder As String = String.Empty
+
+        If files.Length > 0 Then
+
+            prog_bar.Minimum = g_count
+            prog_bar.Maximum = files.Count
+
+            Try
+
+                prog_bar.Visible = True
+
+                For Each file In files
+                    outputFolder = Path.Combine(txtFolderOut.Text, Path.GetFileName(file))
+
+                    If My.Computer.FileSystem.FileExists(file) Then
+                        Continue For
+                    Else
+                        My.Computer.FileSystem.CopyFile(file, outputFolder)
+                    End If
+
+
+                    prog_bar.Value = g_count
+                    'prog_bar.Refresh()
+                    g_count += 1
+
+                Next
+
+                g_count = 1
+                MessageBox.Show("Proceso finalizado con éxito!", "OK", MessageBoxButtons.OK, MessageBoxIcon.None)
+
+            Catch ex As Exception
+
+                MessageBox.Show("Ha ocurrido un error inesperado en el proceso, por favor, ponte en contacto con el servicio técnico.", "ERROR:", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+            End Try
+
+        Else
+
+            MessageBox.Show("La carpeta de origen está vacía." & vbNewLine & "Seleccione otra carpeta que no esté vacía e inténtelo de nuevo.", "ALERTA", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+
+        End If
+
+
+    End Sub
+
+#End Region
+
+#End Region
+
+    ''' <summary>
+    ''' Timer del botón extraer. Lo activa cuando amboz textbox tienen una ruta.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles tmrExtractBtn.Tick
+
+        If (txtFolderIn.TextLength > 0) And (txtFolderOut.TextLength > 0) Then
+            btnExtract.Enabled = True
+        Else
+            btnExtract.Enabled = False
+        End If
+
+    End Sub
+
+
+End Class
